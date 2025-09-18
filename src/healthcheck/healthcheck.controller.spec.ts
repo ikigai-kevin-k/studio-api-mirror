f0fa28248a38ed6bf65ce012cbac46758c4fd828
ikigai-sigma-l <sigma.l@ikigai.team>
2025-09-18 17:25:26 +0800

    fix: unit test
    (cherry picked from commit 2ec975042a334492a5d2b4f1f85a0f2a3665df2c)

--- src/healthcheck/healthcheck.controller.spec.ts
+++ src/healthcheck/healthcheck.controller.spec.ts
@@ -42,7 +42,6 @@ describe('HealthcheckController', () => {
       RoutesEnum.V1_HEALTHCHECK,
       expect.objectContaining({
         schema: expect.any(Object),
-        preHandler: [expect.any(Function)],
       }),
       expect.any(Function),
     );
