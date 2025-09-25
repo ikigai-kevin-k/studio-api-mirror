a349d8a941fad1ac59416fb902a96404ccd9e084
ikigai-sigma-l <sigma.l@ikigai.team>
2025-09-25 10:35:33 +0800

    fix: code order

--- index.ts
+++ index.ts
@@ -1,15 +1,16 @@
-import 'src/otel/instrumentation';
-// instrumentation must be imported first
-import { ModuleManager } from '@ikigaians/mod';
 import tracer from 'dd-trace';
-import { startApp } from 'src/app';
-import { LoggerService } from 'src/log';
-import { StudioMod } from 'src/mod';
-
 tracer.init({
   service: process.env.APP_NAME,
   env: process.env.APP_ENV,
   version: process.env.VERSION || 'unknown',
 });
+
+import 'src/otel/instrumentation';
+// instrumentation must be imported first
+import { ModuleManager } from '@ikigaians/mod';
+
+import { startApp } from 'src/app';
+import { LoggerService } from 'src/log';
+import { StudioMod } from 'src/mod';
 
 startApp(new ModuleManager(LoggerService).registerProfile(...StudioMod));
