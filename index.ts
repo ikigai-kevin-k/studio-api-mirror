2451e0d4e18dd8bec9b9d05827e0b8bf4243ce4f
ikigai-sigma-l <sigma.l@ikigai.team>
2025-09-25 10:22:29 +0800

    fix: datadog trace

--- index.ts
+++ index.ts
@@ -1,8 +1,15 @@
 import 'src/otel/instrumentation';
 // instrumentation must be imported first
 import { ModuleManager } from '@ikigaians/mod';
+import tracer from 'dd-trace';
 import { startApp } from 'src/app';
 import { LoggerService } from 'src/log';
 import { StudioMod } from 'src/mod';
+
+tracer.init({
+  service: process.env.APP_NAME,
+  env: process.env.APP_ENV,
+  version: process.env.VERSION || 'unknown',
+});
 
 startApp(new ModuleManager(LoggerService).registerProfile(...StudioMod));
