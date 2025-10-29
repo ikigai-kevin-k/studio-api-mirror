7ebf14c08d09bf236323d78d7ef88270535a7d7d
Kuosheng Cheng <kuosheng.c@kuosheng.c-MacBook>
2025-10-29 14:55:10 +0800

    refine the initial SQL

---
+++ db-migrations/undo/U1759979280468__initial_schema.sql
@@ -0,0 +1,6 @@
+DROP TABLE IF EXISTS public.studio_status CASCADE;
+DROP TABLE IF EXISTS public.studio_error_signal_log CASCADE;
+DROP TABLE IF EXISTS public.studio_device CASCADE;
+DROP TABLE IF EXISTS public.studio_cdn CASCADE;
+DROP TABLE IF EXISTS public.studio CASCADE;
+
