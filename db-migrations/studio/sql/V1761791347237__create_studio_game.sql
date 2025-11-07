70dc4e5b3d0f3867a2d98d01ab723f517a7f348b
ikigai-sigma-l <sigma.l@ikigai.team>
2025-11-07 15:24:41 +0800

    fix: feedback

--- db-migrations/studio/sql/V1761791347237__create_studio_game.sql
+++ db-migrations/studio/sql/V1761791347237__create_studio_game.sql
@@ -6,8 +6,12 @@ CREATE TABLE IF NOT EXISTS "studio_game" (
     "CURRENT_TABLE_ID" character varying(255), 
     "CREATED_AT" TIMESTAMP(3) NOT NULL DEFAULT now(), 
     "UPDATED_AT" TIMESTAMP(3) NOT NULL DEFAULT now(), 
-    CONSTRAINT "pk_game_id" PRIMARY KEY ("ID")
+    
+    CONSTRAINT "pk_game_id" PRIMARY KEY ("ID"),
+    
+    CONSTRAINT "uq_game_id" UNIQUE ("GAME_ID") 
 ); 
+
 COMMENT ON COLUMN "studio_game"."ID" IS 'Auto increase number'; 
 COMMENT ON COLUMN "studio_game"."GAME_ID" IS 'Unique Game Code'; 
 COMMENT ON COLUMN "studio_game"."PRIMARY_TABLE_ID" IS 'Primary Table ID'; 
@@ -15,7 +19,4 @@ COMMENT ON COLUMN "studio_game"."SECONDARY_TABLE_ID" IS 'Secondary Table ID';
 COMMENT ON COLUMN "studio_game"."CURRENT_TABLE_ID" IS 'Current Table ID'; 
 COMMENT ON COLUMN "studio_game"."CREATED_AT" IS 'When the row was created'; 
 COMMENT ON COLUMN "studio_game"."UPDATED_AT" IS 'When the row was updated';
-
-CREATE INDEX IF NOT EXISTS "idx_game_id" ON "studio_game" ("GAME_ID");
-
 COMMENT ON TABLE "studio_game" IS 'The game table.';
